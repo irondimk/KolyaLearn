dfc8d3ad535f614d68368fdad9cb2a50c53a8e31
Дмитрий Сорокин <irondimk@gmail.com>
2022-12-10 02:29:28 +0700

    shadows and border radius

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const PagesContainer = styled.div`
     background-color: ${({ theme }) => theme.colors.mainBackground()};
     border-radius: ${({ theme }) => theme.decorations.borderRadius.base}px;
+    box-shadow: ${({ theme }) => theme.decorations.boxShadow.default.xs};
 `;
--- src/theme/decorations.ts
+++ src/theme/decorations.ts
@@ -1,14 +1,29 @@
 const decorations = {
     borderRadius: {
-        large: 20,
-        base: 10,
-        min: 4,
-        none: 0,
+        base: 4,
     },
     boxShadow: {
-        base: '0px 8px 21px rgba(53, 63, 71, 0.16)',
-        secondary: '0px 0px 15px rgba(53, 63, 71, 0.25)',
-        dark: '0px 10px 30px rgba(53, 63, 71, 0.1)',
+        default: {
+            xs: '1px 2px 4px rgba(0, 0, 0, 0.1)',
+            s: '2px 4px 8px rgba(0, 0, 0, 0.1)',
+            m: '4px 8px 16px rgba(0, 0, 0, 0.1)',
+            l: '8px 16px 32px rgba(0, 0, 0, 0.1)',
+            xl: '16px 32px 64px rgba(0, 0, 0, 0.1)',
+        },
+        primary: {
+            xs: '1px 2px 4px rgba(255, 102, 51, 0.2)',
+            s: '2px 4px 8px rgba(255, 102, 51, 0.2)',
+            m: '4px 8px 16px rgba(255, 102, 51, 0.2)',
+            l: '8px 16px 32px rgba(255, 102, 51, 0.2)',
+            xl: '16px 32px 64px rgba(255, 102, 51, 0.2)',
+        },
+        secondary: {
+            xs: '1px 2px 4px rgba(112, 192, 91, 0.2)',
+            s: '2px 4px 8px rgba(112, 192, 91, 0.2)',
+            m: '4px 8px 16px rgba(112, 192, 91, 0.2)',
+            l: '8px 16px 32px rgba(112, 192, 91, 0.2)',
+            xl: '16px 32px 64px rgba(112, 192, 91, 0.2)',
+        },
     },
     transition: {
         base: 'all .2s ease-in-out',
